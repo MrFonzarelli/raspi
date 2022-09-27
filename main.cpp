43ae26410c51b1cd9c3274eb4df66870ee39b88b
MrFonzarelli <104815167+MrFonzarelli@users.noreply.github.com>
2022-09-27 13:23:05 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -190,7 +190,7 @@ int main(int argc, char **argv) {
       printf("socket err \n");
       return 0;
   }
-  int res = bind(sfd, (struct sockaddr *) &myaddr, sizeof(myaddr));
+  int res = bind(sfd, (struct sockaddr *) &myaddr, &addr_len);
   if (res == -1) {
       printf("bind err \n");
       return 0;
