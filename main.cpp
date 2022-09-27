ba8942b7df74ff37ce3d2a088ceec349416becb8
MrFonzarelli <104815167+MrFonzarelli@users.noreply.github.com>
2022-09-27 02:09:45 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -41,17 +41,6 @@ struct outGauge {
     int id;
 };
 
-struct sockaddr_in {
-    short sin_family;   // e.g. AF_INET
-    unsigned short sin_port;     // e.g. htons(3490)
-    struct in_addr sin_addr;     // see struct in_addr, below
-    char sin_zero[8];  // zero this if you want to
-};
-
-struct in_addr {
-    unsigned long s_addr;  // load with inet_aton()
-};
-
 int ReverseHandler(void) {
     digitalWrite (pin1, HIGH);
     digitalWrite (pin2, LOW);
@@ -198,7 +187,7 @@ int socket_init(void){
         printf("bind err \n");
         return -1;
     }
-    return 0
+    return 0;
 }
 
 int main(int argc, char **argv) {
