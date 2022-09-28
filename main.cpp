132d87158a1433098f332946f9dea12594b1f594
MrFonzarelli <104815167+MrFonzarelli@users.noreply.github.com>
2022-09-29 00:30:25 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -18,18 +18,18 @@ int pin5 = 5;
 int pin6 = 6;
 int pin7 = 10;
 int pin8 = 11;
-int pin9;
-int pin10;
-int pin11;
-int pin12;
-int pin13;
-int pin14;
-int pin15;
-int pin16;
-int pin17;
-int pin18;
-int pin19;
-int pin20;
+int pin9 = 31;
+int pin10 = 26;
+int pin11 = 27;
+int pin12 = 28;
+int pin13 = 29;
+int pin14 = 25;
+int pin15 = 24;
+int pin16 = 23;
+int pin17 = 22;
+int pindig1 = 14;
+int pindig2 = 13;
+int pindig3 = 12;
 int des_state = 1;
 int cur_state = 0;
 int des_speed = 0;
@@ -191,6 +191,116 @@ int ZeroHandler(void) {
     return 11;
 }
 
+int FirstHandlerTri(void) {
+    digitalWrite (pin1, LOW);
+    digitalWrite (pin2, LOW);
+    digitalWrite (pin3, LOW);
+    digitalWrite (pin4, HIGH);
+    digitalWrite (pin5, LOW);
+    digitalWrite (pin6, LOW);
+    digitalWrite (pin7, HIGH);
+    return 1;
+}
+
+int SecondHandlerTri(void) {
+    digitalWrite (pin1, HIGH);
+    digitalWrite (pin2, LOW);
+    digitalWrite (pin3, HIGH);
+    digitalWrite (pin4, HIGH);
+    digitalWrite (pin5, HIGH);
+    digitalWrite (pin6, HIGH);
+    digitalWrite (pin7, LOW);
+    return 2;
+}
+
+int ThirdHandlerTri(void) {
+    digitalWrite (pin1, HIGH);
+    digitalWrite (pin2, LOW);
+    digitalWrite (pin3, HIGH);
+    digitalWrite (pin4, HIGH);
+    digitalWrite (pin5, LOW);
+    digitalWrite (pin6, HIGH);
+    digitalWrite (pin7, HIGH);
+    return 3;
+}
+
+int FourthHandlerTri(void) { 
+    digitalWrite (pin1, HIGH);
+    digitalWrite (pin2, HIGH);
+    digitalWrite (pin3, LOW);
+    digitalWrite (pin4, HIGH);
+    digitalWrite (pin5, LOW);
+    digitalWrite (pin6, LOW);
+    digitalWrite (pin7, HIGH);
+    return 4;
+}
+
+int FifthHandlerTri(void) {
+    digitalWrite (pin1, HIGH);
+    digitalWrite (pin2, HIGH);
+    digitalWrite (pin3, HIGH);
+    digitalWrite (pin4, LOW);
+    digitalWrite (pin5, LOW);
+    digitalWrite (pin6, HIGH);
+    digitalWrite (pin7, HIGH);
+    return 5;
+}
+
+int SixthHandlerTri(void) {
+    digitalWrite (pin1, HIGH);
+    digitalWrite (pin2, HIGH);
+    digitalWrite (pin3, HIGH);
+    digitalWrite (pin4, LOW);
+    digitalWrite (pin5, HIGH);
+    digitalWrite (pin6, HIGH);
+    digitalWrite (pin7, HIGH);
+    return 6;
+}
+
+int SeventhHandlerTri(void) {
+    digitalWrite (pin1, LOW);
+    digitalWrite (pin2, LOW);
+    digitalWrite (pin3, HIGH);
+    digitalWrite (pin4, HIGH);
+    digitalWrite (pin5, LOW);
+    digitalWrite (pin6, LOW);
+    digitalWrite (pin7, HIGH);
+    return 7;
+}
+
+int EighthHandlerTri(void) {
+    digitalWrite (pin1, HIGH);
+    digitalWrite (pin2, HIGH);
+    digitalWrite (pin3, HIGH);
+    digitalWrite (pin4, HIGH);
+    digitalWrite (pin5, HIGH);
+    digitalWrite (pin6, HIGH);
+    digitalWrite (pin7, HIGH);
+    return 8;
+}
+
+int NinethHandlerTri(void) {
+    digitalWrite (pin1, HIGH);
+    digitalWrite (pin2, HIGH);
+    digitalWrite (pin3, HIGH);
+    digitalWrite (pin4, HIGH);
+    digitalWrite (pin5, LOW);
+    digitalWrite (pin6, HIGH);
+    digitalWrite (pin7, HIGH);
+    return 9;
+}
+
+int ZeroHandlerTri(void) {
+    digitalWrite (pin1, LOW);
+    digitalWrite (pin2, HIGH);
+    digitalWrite (pin3, HIGH);
+    digitalWrite (pin4, HIGH);
+    digitalWrite (pin5, HIGH);
+    digitalWrite (pin6, HIGH);
+    digitalWrite (pin7, HIGH);
+    return 0;
+}
+
 int singleDigitOutput(int state){
     switch(state)
         {
@@ -266,9 +376,79 @@ int tripleDigitOutput(int num) {
     dig1 = num / 100 % 10;
     dig2 = num / 10 % 10;
     dig3 = num % 10;
+    digitalWrite(pindig3, LOW);
+    digitSelect(dig1);
+    digitalWrite(pindig1, HIGH);
+    digitalWrite(pindig1, LOW);
+    digitSelect(dig2);
+    digitalWrite(pindig2, HIGH);
+    digitalWrite(pindig2, LOW);
+    digitSelect(dig3);
+    digitalWrite(pindig3, HIGH);
     cur_speed = des_speed;
-    printf("Speed: %d hundreds %d tens %d units\n", (int)dig1, (int)dig2, (int)dig3);
+    //printf("Speed: %d hundreds %d tens %d units\n", dig1, dig2, dig3);
     return cur_speed;
+}
+
+void digitSelect(int num) {
+    switch(num) {
+        {
+        case 0:
+        {
+            num = ZeroHandlerTri();
+        }
+        break;
+        case 1:
+        {
+            num = FirstHandlerTri();
+        }
+        break;
+        case 2:
+        {
+            num = SecondHandlerTri();
+        }
+        break;
+        case 3:
+        {
+            num = ThirdHandlerTri();
+        }
+        break;
+        case 4:
+        {
+            num = FourthHandlerTri();
+        }
+        break;
+        case 5:
+        {
+            num = FifthHandlerTri();
+        }
+        break;
+        case 6:
+        {
+            num = SixthHandlerTri();
+        }
+        break;
+        case 7:
+        {
+            num = SeventhHandlerTri();
+        }
+        break;
+        case 8:
+        {
+            num = EighthHandlerTri();
+        }
+        break;
+        case 9:
+        {
+            num = NinethHandlerTri();
+        }
+        break;
+        default:
+        {
+            num = NinethHandlerTri();
+        }
+    }
+    return num;
 }
 
 int main(int argc, char **argv) {
@@ -288,6 +468,18 @@ int main(int argc, char **argv) {
   pinMode(pin6, OUTPUT);
   pinMode(pin7, OUTPUT);
   pinMode(pin8, OUTPUT);
+  pinMode(pin9, OUTPUT);
+  pinMode(pin10, OUTPUT);
+  pinMode(pin11, OUTPUT);
+  pinMode(pin12, OUTPUT);
+  pinMode(pin13, OUTPUT);
+  pinMode(pin14, OUTPUT);
+  pinMode(pin15, OUTPUT);
+  pinMode(pin16, OUTPUT);
+  pinMode(pin17, OUTPUT);
+  pinMode(pindig1, OUTPUT);
+  pinMode(pindig2, OUTPUT);
+  pinMode(pindig3, OUTPUT);
    
   myaddr.sin_family = AF_INET;
   myaddr.sin_port = htons(4444);
@@ -323,12 +515,12 @@ int main(int argc, char **argv) {
         }
         distance_traveled += time_delta.count() * speed_to_count / 1000;
         //printf("Distance traveled: %06.1lf km\n", distance_traveled);
-        if (des_state != cur_state) {
-            singleDigitOutput(des_state);
-        }
-        if (des_speed != cur_speed) {
+        //if (des_state != cur_state) {
+            //singleDigitOutput(des_state);
+        //}
+        //if (des_speed != cur_speed) {
             tripleDigitOutput(des_speed);    
-        }
+        //}
         old_time = new_time;
     }
   } while(cur_state !=-1);
