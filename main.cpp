7c7d00a00fa6621b7e8999a2f1edef770774c68e
MrFonzarelli <104815167+MrFonzarelli@users.noreply.github.com>
2022-10-05 21:38:39 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -437,24 +437,14 @@ int tripleDigitOutput(int num) {
     dig1 = num / 100 % 10;
     dig2 = num / 10 % 10;
     dig3 = num % 10;
-    digitalWrite (pindig1, LOW);
+
     digitSelect(dig1);
-    
-    pinMode(pindig1, INPUT);
     pinMode(pindig1, OUTPUT);
-    digitalWrite (pindig1, HIGH);
-    digitalWrite (pindig2, LOW);
     digitSelect(dig2);
-    
-    pinMode(pindig2, INPUT);
     pinMode(pindig2, OUTPUT);
-    digitalWrite (pindig2, HIGH);
-    digitalWrite (pindig3, LOW);
     digitSelect(dig3);
-    
-    pinMode(pindig3, INPUT);
     pinMode(pindig3, OUTPUT);
-    digitalWrite (pindig3, HIGH);
+
 
     cur_speed = des_speed;
     printf("Speed: %d hundreds %d tens %d units\n", dig1, dig2, dig3);
