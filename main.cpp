974144525f6a6a6fb7cc59e340c08ba4187ed82f
MrFonzarelli <104815167+MrFonzarelli@users.noreply.github.com>
2022-10-05 21:54:24 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -440,10 +440,13 @@ int tripleDigitOutput(int num) {
 
     digitSelect(dig1);
     pinMode(pindig1, OUTPUT);
+    digitalWrite(pindig1, LOW);
     digitSelect(dig2);
     pinMode(pindig2, OUTPUT);
+    digitalWrite(pindig2, LOW);
     digitSelect(dig3);
     pinMode(pindig3, OUTPUT);
+    digitalWrite(pindig3, LOW);
 
 
     cur_speed = des_speed;
