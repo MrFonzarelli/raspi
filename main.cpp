9e5cfcbb84ec9d1988bf1386795399872a5d7f5c
MrFonzarelli <104815167+MrFonzarelli@users.noreply.github.com>
2022-10-07 17:13:29 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -443,26 +443,26 @@ int tripleDigitOutput(int num) {
  
     if (dig1 == 0) {
         if (dig2 == 0) {
-            digitalWrite(pindig3, HIGH);   
+            digitalWrite(pindig3, LOW);   
             digitSelect(dig3);
 
             //nanosleep((const struct timespec[]){{0, 150000L}}, NULL);
             //std::this_thread::sleep_for(std::chrono::milliseconds(wait));
-            digitalWrite(pindig3, LOW);
+            digitalWrite(pindig3, HIGH);
         } else {
-            digitalWrite(pindig3, HIGH);   
+            digitalWrite(pindig3, LOW);   
             digitSelect(dig3);
 
             //nanosleep((const struct timespec[]){{0, 150000L}}, NULL);
             //std::this_thread::sleep_for(std::chrono::milliseconds(wait));
-            digitalWrite(pindig3, LOW);
+            digitalWrite(pindig3, HIGH);
             
-            digitalWrite(pindig2, HIGH);   
+            digitalWrite(pindig2, LOW);
             digitSelect(dig2);
 
             //nanosleep((const struct timespec[]){{0, 150000L}}, NULL);
             //std::this_thread::sleep_for(std::chrono::milliseconds(wait));
-            digitalWrite(pindig2, LOW);            
+            digitalWrite(pindig2, HIGH);            
         }
     } else {
         digitalWrite(pindig3, HIGH);   
