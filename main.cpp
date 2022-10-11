f4dd39a3cf90159adf61d40085178d0f60e8618c
fonzarelli <matejmundl@seznam.cz>
2022-10-11 16:18:26 +0200

    commit

--- main.cpp
+++ main.cpp
@@ -723,6 +723,15 @@ int main(int argc, char **argv) {
   digitalWrite(pindig1, HIGH);
   digitalWrite(pindig2, HIGH);
   digitalWrite(pindig3, HIGH);
+
+  digitalWrite (pin1, LOW);
+  digitalWrite (pin2, LOW);
+  digitalWrite (pin3, LOW);
+  digitalWrite (pin4, LOW);
+  digitalWrite (pin5, LOW);
+  digitalWrite (pin6, LOW);
+  digitalWrite (pin7, LOW);
+  digitalWrite (pin8, LOW);
    
   digitalWrite (pin9, LOW);
   digitalWrite (pin10, LOW);
