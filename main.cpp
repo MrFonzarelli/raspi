916bc47cc7d9d598f1da540840ed6de603d573be
fonzarelli <matejmundl@seznam.cz>
2022-10-11 02:48:57 +0200

    commit

--- main.cpp
+++ main.cpp
@@ -649,7 +649,7 @@ void doSingleDigitWork() {
                         digitalWrite (pin5, LOW);
                         digitalWrite (pin6, LOW);
                         digitalWrite (pin7, LOW);
-                        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+                        std::this_thread::sleep_for(std::chrono::milliseconds(200));
                     }
                 }
             }
