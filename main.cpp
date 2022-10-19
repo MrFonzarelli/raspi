9166d2505e4c0b17a9a0eebafaf5fd378d4d978d
fonzarelli <matejmundl@seznam.cz>
2022-10-19 23:06:19 +0200

    commit

--- main.cpp
+++ main.cpp
@@ -944,7 +944,7 @@ int main(int argc, char **argv) {
         outGauge *s = (outGauge *)buffer;
         dashLights = s->showLights;
         if (dashLights != dashLights_old) {
-            printBits(sizeof(dashLights), &dashLights)
+            printBits(sizeof(dashLights), &dashLights);
             dashLights_old = dashLights;
         }
         
