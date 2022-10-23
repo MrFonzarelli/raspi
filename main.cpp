680d95aee21624b81de7e2dc644473cc47b379a7
fonzarelli <matejmundl@seznam.cz>
2022-10-23 18:54:39 +0200

    commit

--- main.cpp
+++ main.cpp
@@ -993,6 +993,7 @@ int main(int argc, char **argv) {
         singleDigitMutex.lock();
         speed = lround(s->speed * 3.6);
         pressure = lround(s->turbo * 10);
+        printf("turbo: %f, pressure: %d\n", s->turbo, pressure);
         distance = dist;
         throttlePos = lround(s->throttle);
         engineTemp = lround(s->engTemp);
