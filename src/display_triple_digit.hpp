dfb3f5d9a07bbe5df01ef60be293cf1886a73660
fonzarelli <matejmundl@seznam.cz>
2022-11-21 18:28:47 +0100

    work

--- src/display_triple_digit.hpp
+++ src/display_triple_digit.hpp
@@ -4,6 +4,17 @@
 namespace Display::TripleDigit
 {
 
+    int firstHandlerTri(void);
+    int secondHandlerTri(void);
+    int thirdHandlerTri(void);
+    int fourthHandlerTri(void);
+    int fifthHandlerTri(void);
+    int sixthHandlerTri(void);
+    int seventhHandlerTri(void);
+    int eighthHandlerTri(void);
+    int ninethHandlerTri(void);
+    int zeroHandlerTri(void);
+    int minusHandlerTri(void);
 }
 
 #endif
