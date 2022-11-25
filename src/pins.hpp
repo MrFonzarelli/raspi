9ac7a779269978fc2089666f3d0b8149575b40b6
fonzarelli <matejmundl@seznam.cz>
2022-11-25 13:52:53 +0100

    work

--- src/pins.hpp
+++ src/pins.hpp
@@ -9,20 +9,20 @@
 #define PIN6 6                     // F
 #define PIN7 10                    // G
 #define PIN8 11                    // DP
-#define PIN9 31                    // 3 digit -- A
-#define PIN10 26                   // 3 digit -- B
-#define PIN11 27                   // 3 digit -- C
-#define PIN12 28                   // 3 digit -- D
-#define PIN13 29                   // 3 digit -- E
-#define PIN14 25                   // 3 digit -- F
-#define PIN15 24                   // 3 digit -- G
+#define PIN9 29                    // 3 digit -- A
+#define PIN10 27                   // 3 digit -- B
+#define PIN11 22                   // 3 digit -- C
+#define PIN12 24                   // 3 digit -- D
+#define PIN13 25                   // 3 digit -- E
+#define PIN14 28                   // 3 digit -- F
+#define PIN15 21                   // 3 digit -- G
 #define PIN16 23                   // 3 digit -- DP
 #define PIN_SCROLL_RIGHT_BUTTON 12 // Scroll display right button
 #define PIN_SCROLL_LEFT_BUTTON 12  // Scroll display left button
 #define PIN_RESET_STAT 8           // Reset odometer button
 #define PIN_CHANGE_UNITS_BUTTON 99 // Change units button
-#define PIN_DIG1 0                 // 3 digit -- dig1
+#define PIN_DIG1 3                 // 3 digit -- dig1
 #define PIN_DIG2 2                 // 3 digit -- dig2
-#define PIN_DIG3 3                 // 3 digit -- dig3
+#define PIN_DIG3 0                 // 3 digit -- dig3
 
 #endif
