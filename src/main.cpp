bcad7bbc428ac65c8fc20cba0a75476f92e7579e
fonzarelli <matejmundl@seznam.cz>
2022-12-01 21:49:08 +0100

    work

--- src/main.cpp
+++ src/main.cpp
@@ -1,74 +1,58 @@
 // includes
 #include "data.hpp"
 #include "io/io.hpp"
-#include "io/display_single_digit.hpp"
-#include "io/display_triple_digit.hpp"
 #include "outgauge.hpp"
-#include "pins.hpp"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <chrono>
-#include <wiringPi.h>
 #include <fstream>
-#include <thread>
-#include <mutex>
-#include <cmath>
 #include <signal.h>
-#include <boost/accumulators/accumulators.hpp>
-#include <boost/accumulators/statistics.hpp>
-#include <boost/accumulators/statistics/rolling_sum.hpp>
-#include <boost/accumulators/statistics/rolling_count.hpp>
-#include <boost/accumulators/statistics/rolling_mean.hpp>
-#include <boost/accumulators/statistics/rolling_window.hpp>
-
-using namespace boost::accumulators;
 
 #define ODOMETER_FILENAME "delete-to-reset-odometer"
 
-void read_odometer()
+void readOdometer()
 {
-    std::ifstream odo_file(ODOMETER_FILENAME);
-    if (odo_file.good())
+    std::ifstream odoFile(ODOMETER_FILENAME);
+    if (odoFile.good())
     {
-        // odo_file >> odometer; TODO
+        // odoFile >> odometer; TODO
     }
     else
     {
         // odometer = 0; TODO
     }
-    odo_file.close();
+    odoFile.close();
 }
 
-void write_odometer()
+void writeOdometer()
 {
-    std::ofstream odo_file(ODOMETER_FILENAME);
-    // odo_file << trip_odometer + odometer; TODO
-    odo_file.close();
+    std::ofstream odoFile(ODOMETER_FILENAME);
+    // odoFile << trip_odometer + odometer; TODO
+    odoFile.close();
 }
 
-void odo_signal_handler(int)
+void odoSignalHandler(int)
 {
-    write_odometer();
+    writeOdometer();
     exit(EXIT_SUCCESS);
 }
 
 int main(int argc, char **argv)
 {
-    signal(SIGINT, odo_signal_handler);
-    signal(SIGTERM, odo_signal_handler);
-    signal(SIGHUP, odo_signal_handler);
-    read_odometer();
+    signal(SIGINT, odoSignalHandler);
+    signal(SIGTERM, odoSignalHandler);
+    signal(SIGHUP, odoSignalHandler);
+    readOdometer();
 
     struct sockaddr_in myaddr, clientaddr;
     memset(&myaddr, 0, sizeof(struct sockaddr_in));
     memset(&clientaddr, 0, sizeof(struct sockaddr_in));
-    socklen_t addr_len = sizeof(struct sockaddr_in);
+    socklen_t addrLen = sizeof(struct sockaddr_in);
     char buffer[128];
     memset(buffer, 0, sizeof(buffer));
 
@@ -76,7 +60,7 @@ int main(int argc, char **argv)
 
     myaddr.sin_family = AF_INET;
     myaddr.sin_port = htons(4444);
-    addr_len = sizeof(myaddr);
+    addrLen = sizeof(myaddr);
     int sfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
     if (sfd == -1)
     {
@@ -90,12 +74,12 @@ int main(int argc, char **argv)
         return 0;
     }
 
-    long long tick_counter = 0;
-    auto old_time = std::chrono::high_resolution_clock::now();
+    long long tickCounter = 0;
+    auto oldTime = std::chrono::high_resolution_clock::now();
 
     while (true)
     {
-        int res = recvfrom(sfd, buffer, sizeof(buffer), 0, (struct sockaddr *)&clientaddr, (socklen_t *)&addr_len);
+        int res = recvfrom(sfd, buffer, sizeof(buffer), 0, (struct sockaddr *)&clientaddr, (socklen_t *)&addrLen);
         if (res == -1)
         {
             printf("recv err \n");
@@ -103,18 +87,18 @@ int main(int argc, char **argv)
         }
         else
         {
-            auto new_time = std::chrono::high_resolution_clock::now();
+            auto newTime = std::chrono::high_resolution_clock::now();
 
             OutGauge *s = (OutGauge *)buffer;
             Data::Tick tickData;
             tickData.outGauge = *s;
-            tickData.tickCounter = tick_counter;
-            tickData.tickTime = (new_time - old_time).count();
+            tickData.tickCounter = tickCounter;
+            tickData.tickTime = (newTime - oldTime).count();
             Data::set(tickData);
 
-            old_time = new_time;
+            oldTime = newTime;
         }
-        tick_counter++;
+        tickCounter++;
     }
 
     return 0;
